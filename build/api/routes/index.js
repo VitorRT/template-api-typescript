3351ad85d9ab16ecf5689bd8d7ce73f55ede9017
VitorRT <barberino.italia@gmail.com>
2023-01-24 14:44:17 -0300

    remove build

--- build/api/routes/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const EmployeesRoutes_1 = __importDefault(require("./EmployeesRoutes"));
-exports.default = (app) => {
-    app.use('/api/v1', EmployeesRoutes_1.default);
-};
--- build/db/connection.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const sequelize_1 = require("sequelize");
-const config_1 = __importDefault(require("./config"));
-const sequelize = new sequelize_1.Sequelize(config_1.default.database, config_1.default.username, config_1.default.password, config_1.default);
-exports.default = sequelize;
